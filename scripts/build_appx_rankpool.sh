54c7c1af1fd71117239a4c6b3206150169a78d0d
iqDF <daniel.thekurniadi@gmail.com>
2019-07-26 20:10:52 +0800

    build scripts of cmake and make cpp

---
+++ scripts/build_appx_rankpool.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+# Author: Daniel Kurniadi
+
+## Define directories
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+PROJ_DIR="$( dirname "${SCRIP_DIR}")"
+
+## Opencv installation directory
+OPENCV_INSTALL_PREFIX="[DEFINE_HERE!]"
+
+## C++ main file
+CPP_MAIN_FILE="${PROJ_DIR}/appxrankpool.cpp"
+
+echo ".. creating build/ directory at $PROJ_DIR/build"
+
+## Delete and recreate build/ dir
+if [ -d build ]; then rm -rf build; fi
+mkdir build && cd build
+
+echo ".. cmake compiling cpp code: $CPP_MAIN_FILE"
+echo ".. cmake using opencv installation build: $OPENCV_INSTALL_PREFIX"
+
+## CMake compile
+cmake -DEXEC_CPP_FILE=$CPP_MAIN_FILE \
+-DOPENCV_CMAKE_INSTALL_PREFIX=$OPENCV_INSTALL_PREFIX ..
+
+## Make install
+make -j8
+
+echo ".. done 
+
