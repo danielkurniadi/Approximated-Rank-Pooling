f56d46ec2ce819fd5e04948882d9bee1b3f49732
iqDF <daniel.thekurniadi@gmail.com>
2019-07-26 12:34:22 +0800

    build scripts fix eof "

--- scripts/build_appx_rankpool.sh
+++ scripts/build_appx_rankpool.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 PROJ_DIR="$( dirname "${SCRIP_DIR}")"
 
 ## Opencv installation directory
-OPENCV_INSTALL_PREFIX="[DEFINE_HERE!]"
+OPENCV_INSTALL_PREFIX="/home/saa2/saa-workplace/opencv_build_files/installation/opencv-3.4.4/share/OpenCV"
 
 ## C++ main file
 CPP_MAIN_FILE="${PROJ_DIR}/appxrankpool.cpp"
@@ -27,5 +27,5 @@ cmake -DEXEC_CPP_FILE=$CPP_MAIN_FILE \
 ## Make install
 make -j8
 
-echo ".. done 
+echo ".. done"
 
