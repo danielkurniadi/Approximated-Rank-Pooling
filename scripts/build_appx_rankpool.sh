68a07e1ea38bd205916576767f8d5f070b29b901
iqDF <daniel.thekurniadi@gmail.com>
2019-08-05 13:31:56 +0800

    [Scripts] fix nit closing " double quotes in echo

--- scripts/build_appx_rankpool.sh
+++ scripts/build_appx_rankpool.sh
@@ -3,7 +3,7 @@
 
 ## Define directories
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
-PROJ_DIR="$( dirname "${SCRIP_DIR}")"
+PROJ_DIR="$( dirname "${SCRIPT_DIR}")"
 
 ## Opencv installation directory
 OPENCV_INSTALL_PREFIX="/home/saa2/saa-workplace/opencv_build_files/installation/opencv-3.4.4/share/OpenCV"
@@ -27,5 +27,5 @@ cmake -DEXEC_CPP_FILE=$CPP_MAIN_FILE \
 ## Make install
 make -j8
 
-echo ".. done"
+echo ".. done 
 
